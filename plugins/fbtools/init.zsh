f3ba770205da7943659ab7b6b1cd4af0eca120de
Allison Wampler <allisonmarw@gmail.com>
2018-01-29 22:00:20 -0800

    .zshrc: move blocks around to pull out generic POSIX things

--- plugins/fbtools/init.zsh
+++ plugins/fbtools/init.zsh
@@ -52,3 +52,13 @@ function dev_server () {
     [[ ${DEV_SERVER} ]] || return 2
 }
 
+
+export FBANDROID_DIR=/Users/aping1/fbsource/fbandroid
+alias quicklog_update=/Users/aping1/fbsource/fbandroid/scripts/quicklog/quicklog_update.sh
+alias qlu=quicklog_update
+
+# added by setup_fb4a.sh
+export ANDROID_SDK=/opt/android_sdk
+export ANDROID_NDK_REPOSITORY=/opt/android_ndk
+export ANDROID_HOME=${ANDROID_SDK}
+export PATH=${PATH}:${ANDROID_SDK}/tools:${ANDROID_SDK}/platform-tools
