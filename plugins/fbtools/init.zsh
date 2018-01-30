29d38d9e9c743e140dbd5f089cf2af8b90e86c0d
Allison Wampler <allisonmarw@gmail.com>
2018-01-29 21:01:28 -0800

    fbtool: bugfixes and added

--- plugins/fbtools/init.zsh
+++ plugins/fbtools/init.zsh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 #
 #
+<<<<<<< HEAD
 _fbtools_local_script="$( cd $(realpath -e $(dirname "${0}")) &>/dev/null; pwd -P;)"
 if [[ $0 == /bin/bash || ! ${_fbtools_local_script} =~ fbtools ]] ; then
     _fbtools_local_script=""
@@ -8,8 +9,8 @@ fi
 #source "${_SCRIPT_DIR}/tasks/init.zsh"
 
 declare -a plugins
-source "${_fbtools_local_script:=${HOME}/.dotfiles/plugins/fbtools}/utils.zsh"
-plugins=( $(find "${_fbtools_local_script}"  -mindepth 2 -name "init.zsh" 2>/dev/null ) )
+source "${_SCRIPT_DIR}/utils.zsh"
+plugins=( $(find "${_SCRIPT_DIR}"  -mindepth 2 -name "init.zsh" 2>/dev/null ) )
 for PLUGIN_FILE in ${plugins[@]}; do
     #printf "Sourcing ${PLUGIN_FILE}..."
     source "${PLUGIN_FILE}"
@@ -36,3 +37,18 @@ function update_iterm_title () {
     _iterm_hooks_rename_both "$(_fb_tmux_helper_get_session)"
 }
 add-zsh-hook periodic update_iterm_title
+=======
+if [[ -d /opt/facebook/hg/share/zsh/site-functions ]]; then
+    fpath+="/opt/facebook/hg/share/zsh/site-functions"
+fi
+
+function dev_server () {
+    DEV_SERVER="$(host $(whoami).sb | head -n1 )"
+    if ! grep -qi NXDOMAIN <<< "$DEV_SERVER" ; then
+        [[ ${DEV_SERVER} ]] || return 1
+        DEV_SERVER="$(echo "${DEV_SERVER}" | awk '{print $NF}' | sed 's/\.$//g')"
+        printf "%s\n" "${DEV_SERVER}"
+    fi
+    [[ ${DEV_SERVER} ]] || return 2
+}
+
