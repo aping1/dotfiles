c7ba19666366c9338e70d9f839cff02f4a787d2f
Allison Wampler <allison.wampler@sony.com>
2019-02-09 12:14:16 -0800

    plugins/init.zsh: add zpython laod (darwin only)

--- plugins/init.zsh
+++ plugins/init.zsh
@@ -1,4 +1,7 @@
 #!/usr/bin/env zsh
-# brew install zpython
+if [[ ${DISTRO} == 'DARWIN' ]] && zgen saved ; then
+	[[ -e /usr/local/lib/zpython ]] || brew install zpython
+fi
 module_path=($module_path /usr/local/lib/zpython)
+zmodload zpython
 
