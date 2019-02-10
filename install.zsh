9fa23293662ecef4a228d99189c016efc93ad19b
Allison Wampler <allison.wampler@sony.com>
2019-02-09 18:25:58 -0800

    .dotfiles: using zgen, tpn from deps/

--- install.zsh
+++ install.zsh
@@ -1,8 +1,10 @@
 #!/usr/bin/env zsh
 # some sanity check
-# setopt xtrace
+setopt xtrace
 
 
+# install .z 
+# TOOD: Run vim +BundleInstall +Qall
 
 unsetopt function_argzero
 
@@ -58,48 +60,54 @@ install_zprezto () {
 [[ -x "${DEPFILES}/fonts/install.sh" ]] && . "${DEPFILES}/fonts/install.sh" && logger -s 'Finished installing install.sh'
 
 ## using .dotfiles
+setopt GLOB_SUBST
 echo ${PWD}
-_TMPR=$(mktemp /tmp/tmp_scripts_XXXX || echo ./temp_script.tmp)
-_TMP_DIFF=$(mktemp /tmp/tmp_diff_XXXX || echo ./temp_diff.tmp)
+_TMPR=$(mktemp /tmp/tmp_scripts_$(date '+%s')_XXXX || echo ./temp_script.tmp)
+_TMP_DIFF=$(mktemp /tmp/tmp_diff_$(date '+%s')_XXXX || echo ./temp_diff.tmp)
 printf '#!/usr/bin/env bash\n' >>${_TMPR}
+printf 'cd ${HOME}\n' >>${_TMPR}
+# printf 'set -e' >>${_TMPR}
 
+# for each row in .dotfiles
 sed -r 's/#.*$//g' ${DOTFILES%/}/.dotfiles | sed '/^$/d;'  | while read GLOB DEST; do
+	# for each glob, dest
 	[[ ${_DEBUG} ]] && printf 'DOTFILES: %s GLOB: %s DEST: %s\n' "${DOTFILES%/}"  "${GLOB}"  "${DEST}"  >&2
-	## Check if dir
+	## Check if the glob is a dir
+	_SRC="${DOTFILES}/${GLOB%/}"
 	if [[ -d ${DOTFILES%/}/${GLOB%/} && ${DEST} ]]; then
-		[[ -e ${DEST} ]] && printf 'Failed to install %s %s\n' "${DOTFILES%/}/${GLOB%/}" "${DEST}"
-		echo ln -sv "${DOTFILES##"${HOME}/"}/${GLOB%/}" "${DEST}"
+		declare -a filelist=($(ls -l ${DOTFILES%/}/${GLOB} | awk '{print $NF}'))
+		# create a link to it as is
+		printf -- '[[ -e "${DEST:h}" ]] || mkdir -p %s\n' "${DEST:h}"
+		echo ln -sv "${_SRC:a}" "${DEST}"
 	else
+		# Glob expansion
 		ls -l ${DOTFILES%/}/${GLOB} | awk '{print $NF}' | \
 			while read this; do
-				unsetopt xtrace
 				rel_this="${this##"${PWD}/"}"
 				DEST=${HOME%/}/${DEST:-${rel_this:t}}
 				if [[ -e $DEST ]] ; then
 				   # If the same or link then rm
-				   setopt xtrace
-			   	   if [[ -h ${DEST} ]]; then
-				     # echo "[No-op] ${DEST} ${HOME%/}/${rel_this}"  >&2
-				     continue 
-				   elif cmp -s "${this}" "${DEST}" ; then
+				   if [[  -h "${DEST}" ]] || cmp -s "${this}" "${DEST}" ; then
+					echo "[No-op] ${DEST} ${HOME%/}/${rel_this}"  >&2
 				     # they are identical, remove the bad one
-				     printf -- 'rm -i %s' "${DEST}"
+				     rm -v "${DEST}" >&2
 				   elif [[ -f ${DEST} ]]; then
 					printf '%s vs %s\n' ${rel_this} ${rel_this:t} >&2
-				     diff -wud ${rel_this} ../${rel_this:t} >> "${_TMP_DIFF}-${rel_this:t}"  
-				     printf -- 'cd %s && git apply -p1 -R %s || exit 1\n' "${DOTFILES}"  "${_TMP_DIFF}-${rel_this:t}"  
+					# Make the dff additive
+					diff -wud ${rel_this} ../${rel_this:t} | sed -E 's/^-([^-]|$)/ \1/g' |  tee /dev/stderr >  "${_TMP_DIFF}-${rel_this:t}"  
+					# rediff compat not implemented
+					# cat =(rediff "${_TMP_DIFF}-${rel_this:t}" ) | tee "${_TMP_DIFF}-${rel_this:t}"   >&2
+				     printf -- 'cd %s && git apply -p1 -R %s || echo Apply failed for $? 1\n' "${DOTFILES}"  "${_TMP_DIFF}-${rel_this:t}"  
 				     printf -- '( cd %s; git diff HEAD -- %s; printf "Revert[Y/n]?" && read -n 1 e; if [[ ${e} =~ [yY] ]]; then git checkout HEAD  -- %s; else rm -i %s; fi)\n' "${DOTFILES}" "${this}" "${this}" "${DEST}"
-				     continue
 				   else
-				     printf 'Failed to install %s %s: Unknown filetype\n' "${DOTFILES%/}/${GLOB%/}" "${DEST}" >&2
+				   	printf 'Failed to install %s %s: Unknown filetype\n' "${DOTFILES%/}/${GLOB%/}" "${DEST}" >&2
 			           fi
-			   	   if ! [[ -h ${DEST} ]]; then
-				     #printf 'Failed to install %s %s: exists\n' "${DOTFILES%/}/${GLOB%/}" "${DEST}" >&2
-				   fi
+				   #printf 'Failed to install %s %s: exists\n' "${DOTFILES%/}/${GLOB%/}" "${DEST}" >&2
 				fi
 				## Doing it
-				echo ln -sv "${rel_this}" "$( if [[ "${DEST}" ]] ; then printf '%s' "${DEST:a}"; fi )" 
+				echo ln -sv .dotfiles/"${rel_this}" "$( if [[ "${DEST}" ]] ; then printf '%s' "${DEST:a}"; fi )" 
 			done
+		unset DEST
 	fi
 done >> "${_TMPR}"
 
