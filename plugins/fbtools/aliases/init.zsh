0210d74373b1a920e0cc44ed032e9a3fbcac9a5f
Allison Wampler <allisonmarw@gmail.com>
2019-05-25 16:39:47 -0700

    Clean task fix, cleanup zshrc, .tmux.conf

--- plugins/fbtools/aliases/init.zsh
+++ plugins/fbtools/aliases/init.zsh
@@ -1,4 +1,7 @@
 
-alias katchin_par='$(project_task_home)/buck.fbcode.datainfra.katchin/cli/katchin.par'
-alias katchin_backend_par='$(project_task_home)/buck.fbcode.datainfra.katchin/backend/katchin_backend.par'
+alias buck_prefix='echo $(project_task_home)/buck-fbcode.fbsource-fbcode.datainfra.katchin'
+alias kobold_buck_prefix='echo $(project_task_home)/buck-fbcode.fbsource-fbcode.kobold.bin'
+alias kobold_par='$(kobold_buck_prefix)/kobold.par'
+alias katchin_par='$(buck_prefix)/cli/katchin.par'
+alias katchin_backend_par='$(buck_prefix)/backend/katchin_backend.par'
 alias db_get='db -r xdb.katchin1 <<< "select concat(\"K\", test_id) from tests where cluster = \"aping1dev\" and time_created between DATE_SUB(NOW(), INTERVAL 1 HOUR) and NOW() order by test_id desc"'
