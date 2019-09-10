8e12adad0e46fe304e38d408f2c8fdf4357882b4
Allison Wampler <aping1@apt-miss.com>
2019-09-09 18:18:28 -0700

    helpers:vimr addd nvim args passthrough to allow for EDITOR -c '...'

--- helpers/helpers.d/01-vimr.zsh
+++ helpers/helpers.d/01-vimr.zsh
@@ -3,6 +3,6 @@ if ! command -v vimr &>/dev/null; then
     echo 'vimr not installed' >&2
     return 1
 else 
-    alias vimr='vimr --cur-env'
+    alias vimr='vimr --cur-env --nvim'
     alias vim='vimr'
 fi
