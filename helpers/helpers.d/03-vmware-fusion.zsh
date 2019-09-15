1879e4e365ae382d8aa2c92db20b6df4f1b2a49a
Allison Wampler <aping1@apt-miss.com>
2019-09-15 10:24:10 -0400

    plugin updates with vmware fusion

--- helpers/helpers.d/03-vmware-fusion.zsh
+++ helpers/helpers.d/03-vmware-fusion.zsh
@@ -4,3 +4,7 @@ export VMWARE_LIB="/Library/Preferences/VMware Fusion"
 alias vm_restartdhcpd='sudo /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --configure ;
 sudo /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --stop;
 sudo /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --start'
+
+path+=(
+"/Applications/VMware Fusion.app/Contents/Library"
+)
