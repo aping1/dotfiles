67e37e67e07779e34606668474bacbdef8f527cc
Allison Wampler <aping1@apt-miss.com>
2019-10-12 20:02:56 -0700

    bootstrap zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,8 +9,10 @@ _trapped() {
     exit $1
 }
 trap '_trapped $?' EXIT 
-git clone --recursive $HOME/dotfiles.git .dotfiles
-cd $HOME/dotfiles.git && git remote -v | grep local && git remote add local file://$HOME/dotfiles.git
 
-export ZPLUG_HOME=/path/to/.zplug
-git clone https://github.com/zplug/zplug $ZPLUG_HOME
+git clone --recursive $HOME/dotfiles.git .dotfiles
+cd $HOME/dotfiles.git && git remote -v | grep local && git remote add local file://$HOME/dotfiles.git || echo
+
+export ZPLUG_HOME=${HOME}/.zplug
+git clone https://github.com/zplug/zplug "${ZPLUG_HOME}"
+
