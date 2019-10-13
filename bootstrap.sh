a3e54a634504b4a524e1b89205a70b3e9df654ef
Allison Wampler <aping1@apt-miss.com>
2019-10-12 19:03:12 -0700

    Docker bootsrtap continue

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,4 +10,7 @@ _trapped() {
 }
 trap '_trapped $?' EXIT 
 git clone --recursive $HOME/dotfiles.git .dotfiles
-cd $HOME/dotfiles.git && git remote add local file://$HOME/dotfiles.git
+cd $HOME/dotfiles.git && git remote -v | grep local && git remote add local file://$HOME/dotfiles.git
+
+export ZPLUG_HOME=/path/to/.zplug
+git clone https://github.com/zplug/zplug $ZPLUG_HOME
