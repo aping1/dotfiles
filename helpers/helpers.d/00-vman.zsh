e7e3ca6c1780d816ba188050d25c306b657f30b2
Allison Wampler <aping1@apt-miss.com>
2020-06-22 10:19:35 -0700

    update vman

--- helpers/helpers.d/00-vman.zsh
+++ helpers/helpers.d/00-vman.zsh
@@ -3,13 +3,11 @@
 SUPERMANDIR="${XDG_CONFIG_HOME:-$HOME/.config}/nvim/plugged/vim-superman/bin"
 if [[ -d ${SUPERMANDIR} ]]; then 
     export PATH="$PATH:${SUPERMANDIR}"
-else
-    echo Failed to load "${SUPERMANDIR}"
+    if command -v vman &>/dev/null ; then
+        alias man="vman"
+        compdef vman="man"
+        # Completion
+        # complete -o default -o nospace -F _man vman
+    fi
 fi
 
-if command -v vman &>/dev/null ; then
-    alias man="vman"
-    compdef vman="man"
-    # Completion
-    # complete -o default -o nospace -F _man vman
-fi
